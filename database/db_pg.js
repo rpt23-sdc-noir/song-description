741a11f9e786adfdfd5653b6d62d94c6f7d064f9
Anatoly Kvitnitsky <Anatoly.Kvitnitsky@gmail.com>
2020-12-15 20:34:06 -0800

    prepare for service deployment; change db info to ec2's

--- database/db_pg.js
+++ database/db_pg.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: 'postgres',
-  password: '',
+  host: process.env.EC2_DB_HOST,
+  user: process.env.EC2_DB_USERNAME,
+  password: process.env.EC2_DB_PW,
   port: 5432,
-  database: 'soundcloud'
+  database: process.env.EC2_DB_NAME,
 });
 
+// hba_config
+// regualar config .cont
 module.exports = pool;
--- pgSeed.js
+++ pgSeed.js
@@ -8,8 +8,8 @@ const bands = require('./bandNames.js');
 const studios = require('./studioNames.js');
 
 const randoNum = () => {
-  const startNum = 1;
-  const endNum = 100;
+  const startNum = 0;
+  const endNum = 99;
   return Math.floor(Math.random() * endNum) + startNum;
 }
 
@@ -24,10 +24,38 @@ const createSeedCsv = () => {
   writer.end();
 }
 
+// const createSeedCsv = () => {
+//   writer.pipe(fs.createWriteStream('seed.csv'));
+//   (async () => {
+//     for (var i = 0; i < recordsInt; i++) {
+//       if (!writer.write({
+//           bandName: bands.bandNames[randoNum()],
+//           description: `Recorded, mixed, and mastered at ${studios.studioNames[randoNum()]} Studios. Produced by ${bands.bandNames[randoNum()]}. © 2020 ${bands.bandNames[randoNum()]}. All rights reserved.`
+//       })) {
+//         await new Promise(resolve => file.once('drain', resolve));
+//       }
+//     }
+//     writer.end();
+//   });
+// }
+
 createSeedCsv();
 
 // pg sql command
 // COPY song(band_name, band_description)
-// FROM '/Users/toly/Desktop/hack_reactor/sdc/song-description/seed.csv'
+// FROM '/Users/toly/Desktop/hack_reactor/sdc/song-description/smallSeed.csv'
 // DELIMITER ','
 // CSV HEADER;
+
+// pg sql command
+// \COPY song(band_name, band_description) FROM '/home/ec2-user/song-description/seed1.csv' DELIMITER ',' CSV HEADER;
+// access postgres from ip
+// cat command sent file one line at a time across http
+// into pg
+
+// vim FILE_NAME
+// type the letter 'i'
+// press 'esc'
+// when done, ':wq!'
+
+// listen for drain event in node
